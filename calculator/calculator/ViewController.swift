7badf0bfda766f6bd83e81c5ff1f2ffd717cbaf3
gosick <f56112000@gmail.com>
2015-10-08 20:28:58 +0800

    add minus multiply divide square

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -27,9 +27,62 @@ class ViewController: UIViewController {
             userIsTypingNow = true
         }
         
-        print("digit is \(digit)")
     }
     
+    var operandStack = Array<Double>()
+    
+    @IBAction func enterKey() {
+        
+        userIsTypingNow = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTypingNow = false
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        
+        if userIsTypingNow {
+            enterKey()
+        }
 
+        switch operation {
+        
+            case "+": performOperation{ $0 + $1 }
+            case "−": performOperation{ $1 - $0 }
+            case "×": performOperation{ $0 * $1 }
+            case "÷": performOperation{ $1 / $0 }
+            case "√": performOperation{ sqrt($0) }
+            default: break
+        }
+        
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enterKey()
+        }
+    }
+    
+    @nonobjc
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enterKey()
+        }
+    }
+    
 }
 
